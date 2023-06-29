93c6778e1bdcdeb00c87ad1e6d6c4ba92da6065c
Giyong8504 <kky5163@naver.com>
2023-06-29 16:33:16 +0900

    수업 복습 정리 업로드

---
+++ Java/수업 복습 정리/day07_20230519_정보 은닉/src/exam02/Ex02.java
@@ -0,0 +1,11 @@
+package exam02;
+
+import exam03.A;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.num2);
+
+    }
+}
---
+++ Java/수업 복습 정리/day07_20230519_정보 은닉/src/exam03/Ex01.java
@@ -0,0 +1,11 @@
+package exam03;
+
+
+//import exam02.Schedule;
+import exam02.*; // 해당 패키지의 전체 클래스를 불러오겠따!
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Schedule s1 = new Schedule();
+    }
+}
