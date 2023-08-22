c87595ee8b9092be4ff9655f3d8550726bb15c2e
Giyong8504 <kky5163@naver.com>
2023-08-22 15:29:42 +0900

    수업 소스 업로드

--- Spring_SpringBoot/spring_study/exam01/src/main/java/exam06/models/member/JoinService.java
+++ Spring_SpringBoot/spring_study/exam01/src/main/java/exam06/models/member/JoinService.java
@@ -1,20 +1,17 @@
 package exam06.models.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class JoinService {
 
-    private MemberDao memberDao;
+    private final MemberDao memberDao;
 
-    private JoinValidator validator;
+    private final JoinValidator validator;
 
-    public JoinService(MemberDao memberDao, JoinValidator validator) {
-        this.memberDao = memberDao;
-
-        this.validator = validator;
-    }
 
     public void join(Member member) {
         validator.check(member);
