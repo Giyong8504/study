5a6611b84ce59ecd4a96f56ab427dc6c5bfad222
Giyong8504 <kky5163@naver.com>
2023-09-05 18:55:21 +0900

    스프링 부트 초기 설정

---
+++ Spring_SpringBoot/spring_study/exam04/src/test/java/org/koreait/Exam04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // junit5, mockito, spring-test /MockMvc 이미 추가 되어있음.
+class Exam04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
