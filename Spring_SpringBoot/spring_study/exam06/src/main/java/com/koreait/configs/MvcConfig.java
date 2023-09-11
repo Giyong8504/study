6c5222655082954be1bee0de22811478528c4dd3
Giyong8504 <kky5163@naver.com>
2023-09-11 20:55:27 +0900

    소스 중간 업로드

---
+++ Spring_SpringBoot/spring_study/exam06/src/main/java/com/koreait/configs/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.koreait.configs;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableJpaAuditing
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() { //반드시 이 이름으로 사용해야 됨.
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons", "messages.validations", "messages.errors");
+
+        return ms;
+    }
+}
---
+++ Spring_SpringBoot/spring_study/exam06/src/main/java/com/koreait/configs/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.koreait.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); //많이 있긴 하지만 이걸 쓴다.
+    }
+}
