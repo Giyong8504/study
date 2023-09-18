068f8217c79dee049ef6854d51994998dc16b23e
Giyong8504 <kky5163@naver.com>
2023-09-18 20:07:06 +0900

    수업 내용 중간 업로드

---
+++ etc/Regex/src/exam01/Ex04.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        String[] mobiles = {
+                "032-320-1234",
+                "016-210-1234",
+                "010-1234-1234"
+                "011-123-123"
+        }
+    }
+}
